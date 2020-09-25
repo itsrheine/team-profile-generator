18501801474892c81e2a982a7d788020e762bc51
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-24 18:24:50 -0700

    building Generator

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -4,4 +4,6 @@ test('creates an employee object', () => {
     const employee = new Employee('Dave');
 
     expect(employee.name).toBe('Dave');
+    expect(employee.id).toEqual(expect.any(Letter, Number));
+    expect(employee.email).toEqual(expect.any(Numbers, Letters, '@'));
 });
