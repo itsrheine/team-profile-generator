e13706de0021e293b8b1234558a21e5d5d6acb5a
Marrione Nguyen <mtiamsic@gmail.com>
2020-09-26 07:12:20 -0700

    4/5 tests passed Employee

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -1,9 +1,34 @@
 const Employee = require('../lib/Employee');
 
 test('creates an employee object', () => {
-    const employee = new Employee('Dave');
+    const employee = new Employee('Dave', '123', 'me@me');
 
     expect(employee.name).toBe('Dave');
-    expect(employee.id).toEqual(expect.any(Letter, Number));
-    expect(employee.email).toEqual(expect.any(Numbers, Letters, '@'));
+    expect(employee.id).toBe('123');
+    expect(employee.email).toBe('me@me');
+});
+
+test(`get's employee name value`, () => {
+    const employee = new Employee('Dave');
+
+    expect(employee.getName()).toBe('Dave');
+});
+
+test(`get's employee ID value`, () => {
+    const employee = new Employee('Dave', 123);
+
+    expect(employee.getID()).toBe(123);
+});
+
+test(`get's employee email value`, () => {
+    const employee = new Employee('Dave', 123, 'me@me');
+
+    expect(employee.getEmail()).toBe('me@me');
+});
+
+test(`get's employee overall stats`, () => {
+    const employee = new Employee('Dave');
+    employee.description = ('Dave', 123, 'me@me');
+
+    expect(employee.getRole()).toEqual(expect.stringContaining(employee.description));
 });
